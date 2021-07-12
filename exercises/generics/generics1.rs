49530f71f5b548ece85eef42580bcb2f5890c6bf
Benji <daylightsavings@gmail.com>
2021-07-11 23:07:06 -0400

    conflict

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,13 +1,6 @@
 // This shopping list program isn't compiling!
 // Use your knowledge of generics to fix it.
 
-<<<<<<< HEAD
-// Execute `rustlings hint generics1` for hints!
-
-// I AM NOT DONE
-
-=======
->>>>>>> benji
 fn main() {
     let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
